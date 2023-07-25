a5e1eceefff41338c7d9d4093633c454694d4b63
fabiodrneles <fabiodrneles@gmail.com>
2023-07-25 14:43:55 -0300

    retirando foto para melhorar o estilo do layout

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,13 +5,13 @@ import Image from "next/image";
 const Avatar = () => {
   return (
     <div className='hidden xl:flex xl:max-w-none'>
-      <Image
+      {/* <Image
         src={'/semfundoG.png'}
-        width={737}
-        height={678}
+        width={700}
+        height={600}
         alt=''
         className='translate-z-0 w-full h-full'
-      />
+      /> */}
     </div>
   );
 };
